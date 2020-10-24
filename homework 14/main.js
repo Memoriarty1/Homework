c181eacd84a7b4d6488490884e5147ff8666ebe0
Ignatenko Igor <ignatenko.igor.ol@gmail.com>
2020-10-24 04:39:28 +0300

    homework 14 with js old clases

--- homework 14/main.js
+++ homework 14/main.js
@@ -55,3 +55,70 @@ obj = { x: parseInt(prompt('Укажите первое число ')), y: parse
 // obj = {};
 const p = new SuperMath();
 p.check(obj);
+
+// function SuperMath() {
+//     // this.x = obj.x
+//     // this.y = obj.y
+//     // this.znak = obj.znak
+
+// }
+// SuperMath.prototype.input = function () {
+//     obj = {
+//         x: parseInt(prompt('Укажите первое число ')),
+//         y: parseInt(prompt('Укажите второе число ')),
+//         znak: prompt('Укажите действие')
+//     }
+//     this.x = obj.x
+//     this.y = obj.y
+//     this.znak = obj.znak
+
+//     p.check()
+// }
+// SuperMath.prototype.check = function () {
+//     switch (this.znak) {
+//         case '/':
+//             if (confirm(`Вы уверены что хотите x${this.znak}y? `) == true) {
+//                 console.log(`${this.x / this.y}`)
+//             } else {
+//                 SuperMath.prototype.input()
+//             }
+//             break
+//         case '+':
+//             if (confirm(`Вы уверены что хотите x${obj.znak}y? `) == true) {
+//                 console.log(`${obj.x + obj.y}`)
+//             } else {
+//                 SuperMath.prototype.input()
+//             }
+//             break
+//         case '-':
+//             if (confirm(`Вы уверены что хотите x${obj.znak}y? `) == true) {
+//                 console.log(`${obj.x - obj.y}`)
+//             } else {
+//                 SuperMath.prototype.input()
+//             }
+
+//             break
+//         case '*':
+//             if (confirm(`Вы уверены что хотите x${obj.znak}y? `) == true) {
+//                 console.log(`${obj.x * obj.y}`)
+//             } else {
+//                 SuperMath.prototype.input()
+//             }
+//             break
+//         case '%':
+//             if (confirm(`Вы уверены что хотите x${obj.znak}y? `) == true) {
+//                 console.log(`${obj.x % obj.y}`)
+//             } else {
+//                 SuperMath.prototype.input()
+//             }
+//             break
+//     }
+// }
+// obj = {
+//     x: 12,
+//     y: 3,
+//     znak: '/'
+// }
+//  p = new SuperMath()
+// p.check(obj)
+// console.log(SuperMath.prototype.check)
