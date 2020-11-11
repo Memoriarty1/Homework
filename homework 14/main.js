56cfaf9b1f47c59317fc3776c8905439b4fa9dab
Ignatenko Igor <ignatenko.igor.ol@gmail.com>
2020-11-11 15:02:43 +0300

    rework homework 14

--- homework 14/main.js
+++ homework 14/main.js
@@ -1,51 +1,40 @@
-//робочий вариант 4 ч утра(после появления ES5 в котором добавили class)
-debugger
-class SuperMath {
-    constructor() {
-    }
-    input() {
-        obj = {
-            x: parseInt(prompt('Укажите первое число ')),
-            y: parseInt(prompt('Укажите второе число ')),
-            znak: prompt('Укажите действие')
-        };
-        this.check(this);
-    }
-    check() {
+
+function SuperMath() {
+    this.check = function (obj) {
         this.x = obj.x;
         this.y = obj.y;
         this.znak = obj.znak;
         switch (this.znak) {
             case '/':
-                if (confirm(`Вы уверены что хотите x${this.znak}y? `) == true) {
+                if (confirm(`Вы уверены что хотите ${this.x}${this.znak}${this.y}? `) == true) {
                     console.log(`${this.x / this.y}`);
                 } else {
                     this.input(this);
                 }
                 break
             case '+':
-                if (confirm(`Вы уверены что хотите x${this.znak}y? `) == true) {
+                if (confirm(`Вы уверены что хотите ${this.x}${this.znak}${this.y}? `) == true) {
                     console.log(`${this.x + this.y}`);
                 } else {
                     this.input(this);
                 }
                 break
             case '-':
-                if (confirm(`Вы уверены что хотите x${this.znak}y? `) == true) {
+                if (confirm(`Вы уверены что хотите ${this.x}${this.znak}${this.y}? `) == true) {
                     console.log(`${this.x - this.y}`);
                 } else {
                     this.input(this);
                 }
                 break
             case '*':
-                if (confirm(`Вы уверены что хотите x${this.znak}y? `) == true) {
+                if (confirm(`Вы уверены что хотите ${this.x}${this.znak}${this.y}? `) == true) {
                     console.log(`${this.x * this.y}`);
                 } else {
                     this.input(this);
                 }
                 break
             case '%':
-                if (confirm(`Вы уверены что хотите x${this.znak}y? `) == true) {
+                if (confirm(`Вы уверены что хотите ${this.x}${this.znak}${this.y}? `) == true) {
                     console.log(`${this.x % this.y}`);
                 } else {
                     this.input(this);
@@ -54,11 +43,19 @@ class SuperMath {
         }
     }
 }
+SuperMath.prototype.input = function () {
+    obj = {
+        x: parseInt(prompt('Укажите первое число ')),
+        y: parseInt(prompt('Укажите второе число ')),
+        znak: prompt('Укажите действие')
+    };
+    this.check(obj);
+}
 obj = {
     x: parseInt(prompt('Укажите первое число ')),
     y: parseInt(prompt('Укажите второе число ')),
     znak: prompt('Укажите действие')
 };
-// obj = {};
+
 const p = new SuperMath();
 p.check(obj);
