08f79c5424fdb40284c4ca9c659f0a40016f3310
Ignatenko Igor <ignatenko.igor.ol@gmail.com>
2020-11-18 17:46:40 +0300

    add homework 21 beta complited v0.1

--- homework 21/main.js
+++ homework 21/main.js
@@ -1,14 +1,25 @@
 window.onload = function () {
     const buttons = document.querySelectorAll('button');
     buttons.forEach(button => {
-        if (button.id === 'buttons') {
+        if (button.id === 'button-counter1' || button.id === 'button-counter2') {
             button.addEventListener('click', counter);
-        } else if(button.id === 'button-cleaner') {
+        } else if (button.id === 'button-cleaner') {
             button.addEventListener('click', clearCounters);
+        } else if (button.id === 'button-set') {
+            button.addEventListener('click', setCounter);
         }
     });
     goOnParsed();
 
+    function setCounter() {
+        let id = prompt("Укажите id блока ");
+        let myValue = +prompt("Укажите число ");
+        if (typeof localStorage.getItem('obj') === 'string') {
+            const parsed = JSON.parse(localStorage.getItem('obj'));
+            parsed[id] = myValue;
+            localStorage.setItem('obj', JSON.stringify(parsed));
+        }
+    }
     function goOnParsed() {
         if (typeof localStorage.getItem('obj') === 'string') {
             const parsed = JSON.parse(localStorage.getItem('obj'));
@@ -16,6 +27,8 @@ window.onload = function () {
                 const elem = document.getElementById(element);
                 elem.value = parsed[element];
             }
+        } else {
+            sirializeFrom(document.querySelector('form'));
         }
     }
 
@@ -33,7 +46,7 @@ window.onload = function () {
     function sirializeFrom(form) {
         let obj = {};
         [].forEach.call(form, element => {
-            if (typeof element.id === 'string' && element.id != 'buttons' && element.id !='button-cleaner') {
+            if (typeof element.id === 'string' && element.id != 'button-counter1' && element.id != 'button-counter2' && element.id != 'button-cleaner' && element.id != 'button-set') {
                 obj[element.id] = element.value;
             }
         });
