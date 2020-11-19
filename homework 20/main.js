fdb03bb95cb0978a3571b157acada515c5dbee99
Ignatenko Igor <ignatenko.igor.ol@gmail.com>
2020-11-19 19:19:25 +0300

    reworked homework 20

--- homework 20/main.js
+++ homework 20/main.js
@@ -20,64 +20,68 @@ window.onload = function () {
         ]
     }
     const actions = {
-        ActionAdd: function () { console.log('ActionAdd')},
-        ActionSaveAs: function () {console.log('ActionSaveAs') },
+        ActionAdd: function () { console.log('ActionAdd') },
+        ActionSaveAs: function () { console.log('ActionSaveAs') },
         ActionExit: function () { console.log('ActionExit') }
     }
     function MenuComponent(config, actions) {
-        console.log(config, '132123')
-        console.log(actions, '132123')
         this.config = config || {};
         this.actions = actions;
         this.container = null;
     }
     MenuComponent.prototype.makeContainer = function () {
         const container = document.createElement('div');
-        container.style.display = 'block'
-        console.log(this.config.name, 'kek')
+        container.style.visibility = 'visible';
         container.classList.add(this.config.type || 'row');
-        console.log(container, 'cont v makecontainer')
+        document.addEventListener('click', function(event){
+            container.style.visibility = 'hidden';
+            event.preventDefault();
+        })
+        document.addEventListener('contextmenu', function(event) {
+            event.preventDefault();
+            container.style.visibility = 'hidden';
+        });
         return container;
     };
-    MenuComponent.prototype.makeItems = function (container) {
+    MenuComponent.prototype.makeItems = function () {
         const { items } = this.config;
         const fragment = document.createDocumentFragment()
         items.forEach(({ title, handler }) => {
-
-            const li = document.createElement('button')
-            console.log(title)
+            const li = document.createElement('button');
             li.innerText = title;
             li.addEventListener('click', this.actions[handler])
-            console.log(li)
             fragment.append(li);
-
         })
         return fragment
     };
 
-    MenuComponent.prototype.render = function () {
+    MenuComponent.prototype.makeMenu = function () {
+        const container = this.makeContainer();
+        const items = this.makeItems();
+        container.append(items);
+        this.container = container;
+        return this;
+    }
+    MenuComponent.prototype.render = function (event) {
         if (this.container) {
-            document.body.append(this.container)
+            let x = event.clientX;
+            let y = event.clientY;
+            this.container.style.left = `${x}px`;
+            this.container.style.top = `${y}px`;
+            document.body.append(this.container);
         }
         return this
     }
-    
-    const menu = new MenuComponent(contextmenu, actions);
-    
-    document.addEventListener('contextmenu', function(event) {
-       
-        menu.makeMenu()
-        .render();
-        event.preventDefault();
-        
-        
-    })
 
-   
-    document.addEventListener('click', keker)
-    function keker(event) {
-        const container = document.querySelector('div')
-        container.style.display = 'none'
+    const menu = new MenuComponent(contextmenu, actions);
+
+    document.addEventListener('contextmenu',function(event) {
+        menu
+        .makeMenu()
+        .render(event);
         event.preventDefault();
-    }
-}
+   
+    });
+
+
+};
